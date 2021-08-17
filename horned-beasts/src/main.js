0f3d201d0b8b8af616ae85016efe846dfca19003
AntoineCharette <antoineocharette@gmail.com>
2021-08-16 21:53:23 -0400

    adds components Header, Main, Footer, and HornedBeast

---
+++ horned-beasts/src/main.js
@@ -0,0 +1,15 @@
+import { Component } from 'react';
+import HornedBeast from './hornedBeast';
+
+class Main extends Component {
+  render() {
+    return (
+      <>
+        <HornedBeast />
+        <HornedBeast />
+      </>
+    );
+  }
+}
+
+export default Main;
